3089118bcc9e7d5cd62308d20647c069040ccba9
Sarah Usher <sarahnusher@gmail.com>
2024-01-31 20:49:38 +0200

    survivor has zero wounds - start test

--- survivor.test.js
+++ survivor.test.js
@@ -4,3 +4,8 @@ test('Survivor has name', () => {
     const survivor = new Survivor("Ford");
     expect(survivor.name).toBe("Ford");
 });
+
+test('Survivor starts with zero wounds', () => {
+    const survivor = new Survivor("Ford");
+    expect(survivor.wounds).toBe(0);
+});
