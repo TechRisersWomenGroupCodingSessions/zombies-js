a38815c39b1c2651a426a509570fffd7af926e13
Emma Gale <emmajgale@hotmail.com>
2024-01-31 18:52:46 +0000

    add 0 wounds completed

--- survivor.js
+++ survivor.js
@@ -1,10 +1,12 @@
 class Survivor {
   constructor(name) {
     this.name = name;
+    this.wounds = 0;
   }
-//  age(x) {
-//    return x - this.year;
-//  }
+
+  //  age(x) {
+  //    return x - this.year;
+  //  }
 }
 
 module.exports = Survivor;
